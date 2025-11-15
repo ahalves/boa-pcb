38f5427ce5453137eb9c0794f2eeca5661e875ae
Adam Hanas-Alves <127245078+ahalves@users.noreply.github.com>
2025-11-15 00:13:56 +0000

    Add files via upload

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,14 +1,24 @@
-int b1 = D3
-int b2 = D4
-int b3 = D5
-int b4 = D6
+#include <Keyboard.h>
 
-int led1 = D10
-int led2 = D9
-int led3 = D8
-int led4 = D7
+int b1 = D3;
+int b2 = D4;
+int b3 = D5;
+int b4 = D6;
+
+int led1 = D10;
+int led2 = D9;
+int led3 = D8;
+int led4 = D7;
+
+unsigned long debounce = 50;
+uint8_t lastState = 0;
+uint8_t stableState = 0;
+unsigned long lastChangeTime = 0;
 
 void setup() {
+
+  Keyboard.begin();
+
   pinMode(b1, INPUT_PULLUP);
   pinMode(b2, INPUT_PULLUP);
   pinMode(b3, INPUT_PULLUP);
@@ -23,14 +33,14 @@ void setup() {
 char decodeChord(uint8_t chord) {
   switch (chord) {
     case 0b1000: return '<';
-    case 0b1000: return '>';
-    case 0b1000: return '+';
-    case 0b1000: return '-';
-    case 0b1000: return '[';
-    case 0b1000: return ']';
-    case 0b1000: return '.';
-    case 0b1000: return ',';
-    case 0b1000: return '\b';
+    case 0b0001: return '>';
+    case 0b0100: return '+';
+    case 0b0010: return '-';
+    case 0b1100: return '[';
+    case 0b0011: return ']';
+    case 0b1010: return '.';
+    case 0b0101: return ',';
+    case 0b1111: return '\b';
     default:     return 0;
   }
 }
@@ -46,15 +56,21 @@ uint8_t readChord() {
   return chord;
 }
 
-int prevState = 0;
-
 void loop() {
-  uint8_t state = readChord();
+  uint8_t currentState = readChord();
 
-  if (state == 0b0000 && prevState != 0) {
-    char out = decodeChord(state);
-    if (out) Serial.print(out); // temporary
+  if (currentState != lastState) lastChangeTime = millis();
+
+  if (millis() - lastChangeTime > debounce) {
+    if (currentState != stableState) {
+      stableState = currentState;
+
+      if (stableState == 0b0000 && lastState != 0b0000) {
+        char out = decodeChord(lastState);
+        if (out) Keyboard.write(out);
+      }
+    }
   }
-  
-  if (state != 0) prevState = state;
+
+  lastState = currentState;
 }
